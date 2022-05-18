1c3051f8d9759c6d944c9d833ce8bb54d1e42fdf
Franserafini <franciscoserafini10@gmail.com>
2022-05-18 18:59:01 -0300

    Agregar archivos de proyecto.

---
+++ prySerafiniPractica1/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace prySerafiniPractica1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdCalcular = new System.Windows.Forms.Button();
+            this.cmdBorrar = new System.Windows.Forms.Button();
+            this.txtNota3 = new System.Windows.Forms.TextBox();
+            this.txtNota2 = new System.Windows.Forms.TextBox();
+            this.txtNota1 = new System.Windows.Forms.TextBox();
+            this.lblNota4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblNota2 = new System.Windows.Forms.Label();
+            this.lblNota1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdCalcular
+            // 
+            this.cmdCalcular.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.cmdCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdCalcular.ForeColor = System.Drawing.SystemColors.Control;
+            this.cmdCalcular.Location = new System.Drawing.Point(163, 244);
+            this.cmdCalcular.Name = "cmdCalcular";
+            this.cmdCalcular.Size = new System.Drawing.Size(100, 37);
+            this.cmdCalcular.TabIndex = 10;
+            this.cmdCalcular.Text = "CALCULAR";
+            this.cmdCalcular.UseVisualStyleBackColor = false;
+            this.cmdCalcular.Click += new System.EventHandler(this.cmdCalcular_Click);
+            // 
+            // cmdBorrar
+            // 
+            this.cmdBorrar.BackColor = System.Drawing.Color.Firebrick;
+            this.cmdBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdBorrar.ForeColor = System.Drawing.SystemColors.Control;
+            this.cmdBorrar.Location = new System.Drawing.Point(25, 244);
+            this.cmdBorrar.Name = "cmdBorrar";
+            this.cmdBorrar.Size = new System.Drawing.Size(100, 37);
+            this.cmdBorrar.TabIndex = 11;
+            this.cmdBorrar.Text = "BORRAR";
+            this.cmdBorrar.UseVisualStyleBackColor = false;
+            this.cmdBorrar.Click += new System.EventHandler(this.cmdBorrar_Click);
+            // 
+            // txtNota3
+            // 
+            this.txtNota3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtNota3.Location = new System.Drawing.Point(163, 154);
+            this.txtNota3.Name = "txtNota3";
+            this.txtNota3.Size = new System.Drawing.Size(100, 20);
+            this.txtNota3.TabIndex = 7;
+            // 
+            // txtNota2
+            // 
+            this.txtNota2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtNota2.Location = new System.Drawing.Point(163, 101);
+            this.txtNota2.Name = "txtNota2";
+            this.txtNota2.Size = new System.Drawing.Size(100, 20);
+            this.txtNota2.TabIndex = 8;
+            // 
+            // txtNota1
+            // 
+            this.txtNota1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtNota1.Location = new System.Drawing.Point(163, 43);
+            this.txtNota1.Name = "txtNota1";
+            this.txtNota1.Size = new System.Drawing.Size(100, 20);
+            this.txtNota1.TabIndex = 9;
+            // 
+            // lblNota4
+            // 
+            this.lblNota4.AutoSize = true;
+            this.lblNota4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota4.Location = new System.Drawing.Point(22, 158);
+            this.lblNota4.Name = "lblNota4";
+            this.lblNota4.Size = new System.Drawing.Size(120, 16);
+            this.lblNota4.TabIndex = 3;
+            this.lblNota4.Text = "INGRESE NOTA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "INGRESE NOTA";
+            // 
+            // lblNota2
+            // 
+            this.lblNota2.AutoSize = true;
+            this.lblNota2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota2.Location = new System.Drawing.Point(22, 101);
+            this.lblNota2.Name = "lblNota2";
+            this.lblNota2.Size = new System.Drawing.Size(120, 16);
+            this.lblNota2.TabIndex = 5;
+            this.lblNota2.Text = "INGRESE NOTA";
+            // 
+            // lblNota1
+            // 
+            this.lblNota1.AutoSize = true;
+            this.lblNota1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNota1.Location = new System.Drawing.Point(22, 44);
+            this.lblNota1.Name = "lblNota1";
+            this.lblNota1.Size = new System.Drawing.Size(120, 16);
+            this.lblNota1.TabIndex = 6;
+            this.lblNota1.Text = "INGRESE NOTA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 304);
+            this.Controls.Add(this.cmdCalcular);
+            this.Controls.Add(this.cmdBorrar);
+            this.Controls.Add(this.txtNota3);
+            this.Controls.Add(this.txtNota2);
+            this.Controls.Add(this.txtNota1);
+            this.Controls.Add(this.lblNota4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNota2);
+            this.Controls.Add(this.lblNota1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdCalcular;
+        private System.Windows.Forms.Button cmdBorrar;
+        private System.Windows.Forms.TextBox txtNota3;
+        private System.Windows.Forms.TextBox txtNota2;
+        private System.Windows.Forms.TextBox txtNota1;
+        private System.Windows.Forms.Label lblNota4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblNota2;
+        private System.Windows.Forms.Label lblNota1;
+    }
+}
+
---
+++ prySerafiniPractica1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySerafiniPractica1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdBorrar_Click(object sender, EventArgs e)
+        {
+            txtNota1.Text = "";
+            txtNota2.Text = "";
+            txtNota3.Text = "";
+        }
+
+        private void cmdCalcular_Click(object sender, EventArgs e)
+        {
+            float nota1 = 0;
+            float nota2 = 0;
+            float nota3 = 0;
+            float promedio = 0;
+
+            nota1 = int.Parse(txtNota1.Text);
+            nota2 = int.Parse(txtNota2.Text);
+            nota3 = int.Parse(txtNota3.Text);
+            promedio = (nota1 + nota2 + nota3) / 3;
+
+            if (promedio > 7)
+            {
+                MessageBox.Show("Promocionaste tu nota es un " + promedio);
+            }
+            if (promedio > 4 && promedio < 7)
+            {
+                MessageBox.Show("Aprobaste tu nota es un " + promedio);
+            }
+            if (promedio < 4)
+            {
+                MessageBox.Show("No aprobaste tu nota es un " + promedio);
+            }
+        }
+    }
+}
